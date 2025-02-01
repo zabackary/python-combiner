6023f8342d545358766b08fb19c2edf48e11c900
zabackary <137591653+zabackary@users.noreply.github.com>
2025-02-01 20:49:25 +0900

    feat: remove python-minifier integration
    It may be added later

--- src/python_minifier/rename/__init__.py
+++
@@ -1,6 +0,0 @@
-from .bind_names import bind_names
-from .mapper import add_namespace
-from .rename_literals import rename_literals
-from .renamer import rename
-from .resolve_names import resolve_names
-from .util import allow_rename_globals, allow_rename_locals
